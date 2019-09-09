a479b6a0b177e0f27a4b8ce0d7d0f418fa01e4a1
wave <986887991@qq.com>
2019-09-09 11:44:20 +0800

    修改vue 3.0.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,34 +17,6 @@
 
 <style lang="less">
   .home {
-    .carousel {
-      height: 400px;
-      width: 100%;
-      border-bottom: 1px solid #5e6983;
-    }
-    .w1200 {
-      height: 450px;
-      .left, .right {
-        width: 50%;
-        p {
-          font-size: 18px;
-          font-weight: bold;
-          line-height: 36px;
-          color: #000000;
-          width: 500px;
-          margin: 100px auto 70px;
-        }
-        .el-button {
-          display: block;
-          width: 200px;
-          height: 40px;
-          margin: 0 0 0 50px;
-        }
-        .el-button--success {
-          margin: 20px 0 0 50px;
-        }
-      }
-    }
-  }
 
+  }
 </style>
